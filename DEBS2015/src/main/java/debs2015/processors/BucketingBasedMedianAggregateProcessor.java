8b2523dd6e004e8e0eba3b966a9af8fc7f978f2c
sacjaya <sachinijayasekara.g@gmail.com>
2015-04-06 11:10:01 +0530

    Improved bucket based median calculation

--- DEBS2015/src/main/java/debs2015/processors/BucketingBasedMedianAggregateProcessor.java
+++ DEBS2015/src/main/java/debs2015/processors/BucketingBasedMedianAggregateProcessor.java
@@ -11,6 +11,9 @@ public class BucketingBasedMedianAggregateProcessor {
 
     float lastReturnedMedian = 0;
 
+    int lastBucketIndex;
+    int bucketChainIndex;
+
     public float getMedian() {
 
         if (totalElements % 2 == 0) {
@@ -70,17 +73,89 @@ public class BucketingBasedMedianAggregateProcessor {
               if (roundValue < size - 1) {
                  mediationArray[roundValue] += 1;
              } else {
+                 roundValue = 10001;
                  mediationArray[size - 1] += 1;
              }
              totalElements++;
 
-             return getMedian();
+             return addAndGetMedian(roundValue);
          }
+    }
+
+    private float addAndGetMedian(int roundValue){
+        float returnVal;
+        if(totalElements==1){
+            lastBucketIndex = roundValue;
+            bucketChainIndex = 1;
+            returnVal =  roundValue;
+        } else if(totalElements%2 == 0){ // even
+             if(lastBucketIndex>roundValue){
+                 returnVal = (lastBucketIndex+getBefore())/200; //Done
+             } else {
+                 returnVal =  (lastBucketIndex+getNext(false))/200;  //done
+             }
+        }  else {
+            if(lastBucketIndex>roundValue){
+                returnVal =  (lastBucketIndex)/100;  //done
+            } else {
+                returnVal =  (getNext())/100;   //done
+            }
+        }
+
+        lastReturnedMedian = returnVal;
+        return returnVal;
+    }
+
+
+
+    private int getNext(){
+      if(mediationArray[lastBucketIndex]>bucketChainIndex){
+                  bucketChainIndex++;
+                  return  lastBucketIndex;
+      }  else {
+          for(int i= lastBucketIndex+1;i<size;i++){
+              if(mediationArray[i]>0) {
+                  lastBucketIndex = i;
+                  bucketChainIndex = 1;
+                  return i;
+              }
+          }
+      }
+        return  lastBucketIndex;
+    }
+
+    private int getNext(boolean keepLastValues){
+        if(mediationArray[lastBucketIndex]>bucketChainIndex){
+            return  lastBucketIndex;
+        }  else {
+            for(int i= lastBucketIndex+1;i<size;i++){
+                if(mediationArray[i]>0) {
+                    return i;
+                }
+            }
+        }
+        return  lastBucketIndex;
+    }
+
+
+    private int getBefore(){
+        if(bucketChainIndex>1){
+            bucketChainIndex--;
+            return  lastBucketIndex;
+        }  else {
+            for(int i= lastBucketIndex-1;i<size;i--){
+                if(mediationArray[i]>0) {
+                    lastBucketIndex = i;
+                    bucketChainIndex = mediationArray[i];
+                    return i;
+                }
+            }
+        }
+        return  lastBucketIndex;
     }
 
 
     public float processRemove(float element) {
-
         if(element<0){
             return lastReturnedMedian;
         } else {
@@ -88,12 +163,38 @@ public class BucketingBasedMedianAggregateProcessor {
             if (roundValue < size - 1) {
                 mediationArray[roundValue] -= 1;
             } else {
+                roundValue = 10001;
                 mediationArray[size - 1] -= 1;
             }
             totalElements--;
 
-            return getMedian();
+            return removeAndGetMedian(roundValue);
         }
+    }
+
+
+    private float removeAndGetMedian(int roundValue){
+        float returnVal;
+        if(totalElements==0){
+            lastBucketIndex = 0;
+            bucketChainIndex = 0;
+            returnVal =  0;
+        } else if(totalElements%2 == 0){ // even
+            if(lastBucketIndex<roundValue){
+                returnVal = (lastBucketIndex+getBefore())/200; //Done
+            } else {
+                returnVal =  (lastBucketIndex+getNext(false))/200;  //done
+            }
+        }  else {
+            if(lastBucketIndex<roundValue){
+                returnVal =  (lastBucketIndex)/100;  //done
+            } else {
+                returnVal =  (getNext())/100;   //done
+            }
+        }
+
+        lastReturnedMedian = returnVal;
+        return returnVal;
     }
 
 //    public void removeElement(float element) {
